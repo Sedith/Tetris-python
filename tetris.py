5b3bc3fde9bd2596d72805b77a0219467d1c03dc
mjacquet <mjacquet@laas.fr>
2019-04-01 00:01:13 +0200

    tetra functional

--- tetris.py
+++ tetris.py
@@ -1,9 +1,11 @@
+####################################################################################################
 ### Imports
 import numpy as np
 import time
 import random
 
-################################################################################
+
+####################################################################################################
 ### Tetraminos
 T = 1 ; S = 2 ; Z = 3 ; J = 4 ; L = 5 ; I = 6 ; O = 7
 
@@ -24,7 +26,7 @@ class Tetra:
     @property
     def shape(self): return self._shape
     @shape.setter
-    def shape(self,shape): self._shape = shape
+    def shape(self,s): self._shape = s
 
     ### Function
     def _shape_from_color(self, color, rot  = 0):
@@ -54,55 +56,60 @@ class Tetra:
         pose = []
         for (i,j),cell in np.ndenumerate(self.shape):
             if cell != 0:
-                if pose == []: pose = np.matrix([self.anchor[0]+i, self.anchor[1]+j]).transpose()
-                else         : pose = np.hstack([pose, np.matrix([self.anchor[0]+i, self.anchor[1]+j]).transpose()])
+                pose.append((self.anchor[0]+i, self.anchor[1]+j))
         return pose
+
+    def get_color(self): return np.amax(self.shape)
 
     def rot_l(self, n = 1): self.shape = np.rot90(self.shape,n)
     def rot_r(self, n = 1): self.shape = np.rot90(self.shape,-n)
 
     def _move(self, move):
         assert move == (0,1) or move == (1,0) or move == (0,-1)
-        print (self.anchor)
-        self.anchor += move
-        print (self.anchor)
+        self.anchor = (self.anchor[0]+move[0], self.anchor[1]+move[1])
 
     def fall(self): self._move((1,0))
     def move_l(self): self._move((0,-1))
     def move_r(self): self._move((0,1))
 
+
+####################################################################################################
 ### Board
-rows = 20
-cols = 10
-def new_board(rows = rows,cols = cols):
-    return np.zeros((rows,cols), dtype=np.uint8)
+def new_board(rows = 20,cols = 10):
+    return np.full((rows,cols), '-', dtype=np.unicode_)
 
 def remove_line(board, i):
     return np.delete(board, i, axis=0)
 
-def add_line(board):
+def fill_board(board, rows = 20):
     assert board.shape[0] < rows
-    return np.concatenate((np.zeros((1,cols), dtype=np.uint8),board), axis=0)
+    return np.concatenate((np.zeros((rows-board.shape[0] ,cols), dtype=np.uint8),board), axis=0)
 
-# def create_tetra(board, tetras = tetraminos, start = None):
-#     if start is None: start = random.randint(round(board.shape[1]*40/100), round(board.shape[1]*70/100)) - round(len(tetra[0])-1)
-#     for i,row in enumerate(tetra):
-#         for j,cell in enumerate(row):
-#             board[i,start+j] = cell
-#     return (board,(0,start))
+def create_tetra(board, tetra):
+    pose = tetra.get_full_pose()
+    for cell in pose: board[cell] = '0'
+    return board
 
+def remove_tetra(board, tetra):
+    pose = tetra.get_full_pose()
+    for cell in pose: board[cell] = '-'
+    return board
 
-################################################################################
+####################################################################################################
 ### Main
-board = new_board()
-tetra = Tetra(L)
-tetra.move_l()
-# print (board)
+rows = 20
+cols = 10
 
-# while 1:
-#     time.sleep(1)
-#     move = (0,1)
-#     board, pose = move_tetra(board, tetra, pose, move)
-#     print(board)
+board = new_board()
+tetra = Tetra()
+board = create_tetra(board,tetra)
+print(board)
+
+while 1:
+    time.sleep(1)
+    board = remove_tetra(board, tetra)
+    tetra.fall()
+    board = create_tetra(board, tetra)
+    print(board, '\n\n\n')
 #
 #
